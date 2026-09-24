c346b633fa832bfa95ade833181b2a8665b45e33
Jeffer <jeffersond.condizaq@uqvirtual.edu.co>
2026-09-23 22:33:49 -0500

    first commit

---
+++ src/main/java/com/jefferson/notifications/DevPlusMain.java
@@ -0,0 +1,10 @@
+package com.jefferson.notifications;
+
+public class DevPlusMain {
+    static void main() {
+
+        String menu= "Que desea realizar hoy?\n" +
+                "\n";
+
+    }
+}
